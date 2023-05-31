832c697e67c202473b01cf29dc79c422cafd30e7
casaDelFuego <tselyuk.olga@gmail.com>
2023-05-31 21:13:01 +0200

    aurora effect for background

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,7 @@ export default {
       return routes[this.currentPath.slice(1) || '/'] || NotFound
     }
   },
-  mounted() {
-    window.addEventListener('hashchange', () => {
-      this.currentPath = window.location.hash
-    })
-  }
+
 }
 </script>
 
@@ -32,4 +28,29 @@ export default {
   <a href="#/about">About</a> |
   <a href="#/non-existent-path">Contact</a>
   <component :is="currentView" />
+  <div class="aurora" ref="auroraRef"></div>
 </template>
+
+<style scoped>
+.aurora {
+  width: 100%;
+  height: 300vh;
+  background: linear-gradient(45deg, #1a1a1a, #8a008a, #ff69b4, #ff1493);
+  background-size: 200% 200%;
+  animation: auroraAnimation 5s ease-in-out infinite;
+}
+
+@keyframes auroraAnimation {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
